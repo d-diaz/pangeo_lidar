f6631e4e089fc8bc641cff143ca367a5254d5971
David Diaz <ddiaz@ecotrust.org>
2019-02-10 01:22:08 -0800

    Moved jupyter extension installs to start.sh

--- binder/start.sh
+++ binder/start.sh
@@ -1,5 +1,13 @@
 # #!/bin/bash
 
+# pangeo-related configuration for jupyter
+jupyter serverextension enable --py nbserverproxy --sys-prefix
+jupyter labextension install @jupyter-widgets/jupyterlab-manager \
+    @jupyterlab/hub-extension@0.12 \
+    @pyviz/jupyterlab_pyviz \
+    jupyter-leaflet \
+    dask-labextension
+
 # Replace DASK_DASHBOARD_URL with the proxy location
 sed -i -e "s|DASK_DASHBOARD_URL|/user/${JUPYTERHUB_USER}/proxy/8787|g" \
   ~/binder/jupyterlab-workspace.json
